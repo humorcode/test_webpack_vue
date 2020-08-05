ae8a54d400e4c8e9ad59465ea65b85995b8c353b
cuiyj <cuiyj>
2020-08-05 12:49:27 +0800

    css,image

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,20 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname,'dist')
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [
+                'style-loader',
+                'css-loader'
+            ]
+        }, {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: [
+                'file-loader'
+            ]
+        }]
     }
-}
+};
