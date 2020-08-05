d67f474f1e5bf80b3c2c42cfb922572b38125c5c
cuiyj <cuiyj>
2020-08-05 14:56:51 +0800

    TEST

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './icon.png';
+import Data from './data.xml';
 
 function component() {
     var element = document.createElement('div');
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hello', 'webpack', '哈哈'], ', ,');
     element.classList.add('hello');
 
     var myIcon = new Image();
     myIcon.src = Icon;
     element.appendChild(myIcon);
-
+    console.log(Data);
     return element;
 }
 document.body.appendChild(component());
