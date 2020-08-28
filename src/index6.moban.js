3841965c6c3ccd1384b273f2f40bfa27accae1a6
cuiyj <cuiyj>
2020-08-29 02:54:26 +0800

    处理边界情况,模板定义的替代品

--- src/index6.moban.js
+++ src/index6.moban.js
@@ -2,3 +2,14 @@ import Vue from './vue.js'
 export default function print() {
     return 'vue function';
 }
+
+Vue.component("my-inline-template", { template: `<p>
+<slot>默认-内联模板</slot>
+</p>` });
+Vue.component('my-x-template', {
+    template: '#hello-world-template'
+})
+new Vue({
+    el: "#mobandingyitab",
+    data: {}
+});
