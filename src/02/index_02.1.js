e879c81966c0186df75cd21eec5213ef4fb408fa
cuiyj <cuiyj>
2020-09-02 17:57:46 +0800

    过滤器

--- src/02/index_02.1.js
+++ src/02/index_02.1.js
@@ -2,3 +2,22 @@ import Vue from './../vue.js'
 export default function print() {
     return 'vue function';
 }
+
+//4.过滤器===
+Vue.filter('capitalize', function(value) {
+    if (!value) return ''
+    value = value.toString()
+    return value.charAt(0).toUpperCase() + value.slice(1)
+})
+new Vue({
+    data: {
+        items: ['a', 'b', 'c']
+    },
+    filters: {
+        capitalize: function(value) {
+            if (!value) return ''
+            value = value.toString()
+            return value.charAt(0).toUpperCase() + value.slice(1)
+        }
+    }
+})
